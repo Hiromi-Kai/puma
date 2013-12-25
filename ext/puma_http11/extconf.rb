7c8f170a11e4895d8eac058baef51f5dcbe8cc2d
Hiromi Kai <pie05041008@gmail.com>
2013-12-25 20:02:28 +0900

    add gdi32 link to build option

--- ext/puma_http11/extconf.rb
+++ ext/puma_http11/extconf.rb
@@ -4,5 +4,6 @@ dir_config("puma_http11")
 
 $defs.push "-Wno-deprecated-declarations"
 $libs += " -lssl -lcrypto "
+$libs += " -lgdi32 " if RUBY_PLATFORM =~ /mingw/
 
 create_makefile("puma/puma_http11")
